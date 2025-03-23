2c8b4b296bc59a0824d9300915eb28df067bced4
Exploring-glitch <deysasha262@gmail.com>
2025-03-23 12:33:45 +0530

    Made changes

--- app.js
+++ app.js
@@ -1,56 +1,93 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const app = express();
+const bcrypt = require("bcrypt");
+const { z } = require("zod");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "2604SrEeJaDeY"
 const port = 3005;
-const JWT_SECRET = "sreejalovesyash";
 
-let users=[];
+const {UserModel , TodoModel} = require("./db"); //importing the UserModel and TodoModel from db.js
 
-app.use(express.json());  //middleware which is needed whenever we use req.body
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://admin:mu9wbw2g8TBgJBVx@cluster0.tgsxo.mongodb.net/todo-app-database"); //connect to the given MongoDB cluster and the database (/todo-app-database)
+
+app.use(express.json()); //middleware which is needed whenever we use req.body
+
+
+
 
 app.get("/", function(req,res){  //if we write "http://localhost:3005" in browser, it will show the website frontend in the browser
     res.sendFile(__dirname + "/public/index.html");
 })
 
-app.post("/signup", function(req,res){  //to create a username and password and signup
-    const username = req.body.username;
+app.post("/signup", async function(req,res){  
+    const requiredBody = z.object({ //We make a schema and specify how the object will look like and add input validations to it
+        email: z.string().email("Email must be in correct format").max(100).min(5, "Email must contain at least characters"),
+        password: z.string().max(50).min(5, "Password must contain atleast 5 characters").regex(/[A-Z]/,"Password must contain at least one uppercase letter").regex(/[a-z]/, "Password must contain at east one lowercase letter").regex(/[\d]/, "Password must contain at least one digit").regex(/[@$!%?&]/, "Password must contain at least one special character"),
+        name: z.string().max(50).min(3, "Name must contain at least 3 characters")
+    })
+    const paresedDataWithSuccess = requiredBody.safeParse(req.body); //here we connect the users entered data(req.body) with our schema(requiredBody) and safeParse it
+    if(!paresedDataWithSuccess.success){ //if somethinf is wrong it will show error
+        return res.status(400).json({
+            errors: paresedDataWithSuccess.error.errors.map(err => err.message)
+        });
+    }
+    const email = req.body.email;
     const password = req.body.password;
+    const name = req.body.name;
 
-    users.push ({ 
-        username: username,
-        password: password
-    })
+    let errorThrown = false;
 
-    res.json({
-        message: "You have successfully signed up"
-    })
+    try{
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-    console.log(users);
+        await UserModel.create({ //we need to await because this is an asynchronous function which returns a promise 
+            email: email,
+            password: hashedPassword,
+            name: name
+        })
+    }catch{
+        res.status(403).json({
+            message : "User already exists"
+        })
+        errorThrown = true;
+    }
+
+    if(!errorThrown){
+        res.json({
+            message: "You have successfully signed up"
+        }) 
+    }
 })
 
-app.post("/signin", function(req,res){  //to check if usename and password is correct, if yes then sign in and give a token to the user
-    const username = req.body.username;
+
+app.post("/signin", async function(req,res){  //to check if usename and password is correct, if yes then sign in and give a token to the user
+    const email = req.body.email;
     const password = req.body.password;
-    
-    let foundUser = null;
-    for(let i=0; i<users.length; i++){
-        if(users[i].username === username && users[i].password === password){
-            foundUser= users[i];
-        }
+
+    const user = await UserModel.findOne({
+        email: email
+    })
+
+    if(!user){
+        res.status(403).jsonjson({
+            message: "Incorrect credentials"
+        })
     }
 
-    if(!foundUser){
-        res.status(404).json({
-            message: "Invalid username or password"
+    const passwordCompare = await bcrypt.compare(password, user.password);
+
+    if(passwordCompare){
+        const token = jwt.sign({ id: user._id.toString() }, JWT_SECRET); //create a token with encoded user id
+        res.json({
+            token: token //return the token
         })
+        res.header("jwttoken", token);
     }
     else{
-        const token = jwt.sign({ username : foundUser.username }, JWT_SECRET);
-        res.json({
-            token: token,
-            message: "login successful"
+        res.status(403).json({
+            message: "Incorrect credentials"
         })
-        res.header("jwt", token);
     }
 })
 
---
+++ db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose"); 
+const Schema = mongoose.Schema; 
+const ObjectId = mongoose.ObjectId;
+
+const User = new Schema({ //create a basic schema(structure) of how your database will look like
+    email: { type: String, unique: true }, //the email must be unique i.e, two users cannot login with same email
+    password: String,
+    name: String
+})
+const Todo = new Schema({
+    userId: ObjectId,
+    title: String,
+    done: Boolean,
+})
+UserModel = mongoose.model('users', User); //create a model where the schema for the users database is User so it stores the data in the users
+TodoModel = mongoose.model('todos', Todo);
+
+
+module.exports = { //this is used to export in js, so that we can import and use the Usermodel and Todomodel in other files (index.js).
+    UserModel: UserModel,
+    TodoModel: TodoModel
+}
+    
